70ada6b70e8eb65394d8691021dc70b8c122a207
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-07-04 18:16:56 +0530

    template-cli local run successful

---
+++ createDirectoryContents.js
@@ -0,0 +1,60 @@
+import * as fs from "fs";
+import process from "process";
+import { execSync } from "child_process";
+
+const CURR_DIR = process.cwd();
+
+const runCommand = (command) => {
+	try {
+		execSync(`${command}`, { stdio: "inherit" });
+	} catch (error) {
+		console.error(`Failed to execute ${command}`, error);
+	}
+};
+
+const createDirectoryContents = (
+	templatePath,
+	newProjectPath,
+	installPackage
+) => {
+	function start(templatePath, newProjectPath) {
+		const filesToCreate = fs.readdirSync(templatePath);
+
+		filesToCreate.forEach((file) => {
+			const origFilePath = `${templatePath}/${file}`;
+
+			// get stats about the current file
+			const stats = fs.statSync(origFilePath);
+
+			if (stats.isFile()) {
+				const contents = fs.readFileSync(origFilePath, "utf8");
+
+				// Rename
+				if (file === ".npmignore") file = ".gitignore";
+
+				const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+				fs.writeFileSync(writePath, contents, "utf8");
+			} else if (stats.isDirectory()) {
+				fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+				// recursive call
+				start(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+			}
+		});
+	}
+
+	console.log("Downloading Rivant Media Next Template");
+	start(templatePath, newProjectPath);
+
+	console.log("Installing Dependencies");
+	const installDepsCommand = `cd ${newProjectPath} && ${installPackage} install`;
+	const installDeps = runCommand(installDepsCommand);
+	if (!installDeps) process.exit(-1);
+
+	console.log(
+		"Congratulations! You are ready. Follow the following commands to start"
+	);
+	console.log(`cd ${newProjectPath} && ${installPackage} dev`);
+};
+
+export default createDirectoryContents;
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import * as fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import process from "process";
+
+import createDirectoryContents from "./createDirectoryContents.js";
+const CURR_DIR = process.cwd();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const template = fs.readdirSync(`${__dirname}/template`)[0];
+
+const QUESTIONS = [
+	{
+		name: "project-name",
+		type: "input",
+		message: "Project name:",
+		validate: function (input) {
+			if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+			else
+				return "Project name may only include letters, numbers, underscores and hashes.";
+		}
+	},
+	{
+		name: "package-choice",
+		type: "list",
+		message: "Which installation package manager would you like to choose?",
+		choices: ["npm", "pnpm", "yarn"]
+	}
+];
+
+inquirer.prompt(QUESTIONS).then((answers) => {
+	const packageChoice = answers["package-choice"];
+	const projectName = answers["project-name"];
+	const templatePath = `${__dirname}/template/${template}`;
+
+	fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+	createDirectoryContents(templatePath, projectName, packageChoice);
+});
